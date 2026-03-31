b688979246965f09fcf1b0a680c03ebd4f7f08b0
Spring <sergiodela2006@gmail.com>
2026-03-31 23:22:07 +0200

    WinZone añadida

--- bi-dimensional/scripts/tile_map.gd
+++ bi-dimensional/scripts/tile_map.gd
@@ -11,3 +11,8 @@ func resetColor():
 	invertido = false
 	if material:
 		material.set_shader_parameter("invert", invertido)
+
+
+func _on_win_zone_body_entered(body: Node2D) -> void:
+	if body.name == "fedeP":
+		print("Felicidades has ganado.")
