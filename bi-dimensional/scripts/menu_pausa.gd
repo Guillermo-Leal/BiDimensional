f32778ff18f714c4c404bcade88e2ba8bf19ba1b
Guillermo-Leal <guillermo.lealjimenez1@gmail.com>
2026-06-09 23:04:11 +0200

    Menu de pausa y ultimos niveles

---
+++ bi-dimensional/scripts/menu_pausa.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Pausar"):
+		if get_tree().paused: 
+			visible = false
+			get_tree().paused = false
+		else: 
+			visible = true
+			get_tree().paused = true
+			
+
+func _on_volver_al_menu_principal_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/Niveles/Menus/MenuPrincipal.tscn")
+
+
+func _on_continuar_pressed() -> void:
+	visible = false
+	get_tree().paused = false
