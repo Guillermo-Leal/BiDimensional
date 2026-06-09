11e0652bd2f072a028a3358f10754934f971cb8b
Guillermo-Leal <guillermo.lealjimenez1@gmail.com>
2026-06-09 18:24:41 +0200

    Menu de opciones con sliders para volumen y brillo

---
+++ bi-dimensional/scenes/ajustes.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_volver_al_menu_principal_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Niveles/Menus/MenuPrincipal.tscn")
--- bi-dimensional/scripts/menu_principal.gd
+++ bi-dimensional/scripts/menu_principal.gd
@@ -5,7 +5,7 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	print("aun no funca")
+	get_tree().change_scene_to_file("res://scenes/Niveles/Menus/Ajustes.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
