1b7e114691cfe127ac02d7115bce4e41ccd949fd
Spring <sergiodela2006@gmail.com>
2026-06-09 15:20:33 +0200

    Pantalla de créditos

--- bi-dimensional/scripts/menu_principal.gd
+++ bi-dimensional/scripts/menu_principal.gd
@@ -9,3 +9,6 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_info_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Niveles/Menus/Creditos.tscn")
