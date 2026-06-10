d7f6db4e377c4789b70613574022d05fb7db402a
Spring <sergiodela2006@gmail.com>
2026-06-10 11:41:24 +0200

    Comentado script de puerta

--- bi-dimensional/scripts/puerta.gd
+++ bi-dimensional/scripts/puerta.gd
@@ -4,20 +4,20 @@ extends Area2D
 @onready var sprite = $Sprite2D
 signal nivel_completado
 
-func _ready():
+func _ready(): #La puerta comienza sin estar activada, monitoring hace que se active la winzone.
 	monitoring = false
 
-func abrir_puerta():
+func abrir_puerta(): #Al recoger la llave o pulsar el botón, la winzone se activa y se cambia su sprite.
 	sprite.texture = preload("res://sprites/puerta_abierta.png")
 	abrirpuerta.play()
 	monitoring = true
 
-func cerrar_puerta():
+func cerrar_puerta(): #En caso de que se suelte el boton, se desactiva y se cierra.
 	sprite.texture = preload("res://sprites/puerta_cerrada.png")
 	monitoring = false
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void: #Winzone.
 	print("Intentando entrar")
 	if body.name == "fedeP":
 		print("Has ganado")
